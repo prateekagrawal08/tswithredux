0001ba5acdf85c5e284103247081b072d077b009
Prateek Aggarwal <prateekagrawal08@gmail.com>
2024-04-16 02:16:50 +0530

    promise examples

--- src/api/examples-promise.js
+++ src/api/examples-promise.js
@@ -1,15 +1,23 @@
-
 export const promiseExample = () => {
-    const p1 = Promise.resolve(50);
-    const p2 = 200;
-    const p3 = new Promise((resolve, reject)=> {
-        setTimeout(()=>{
-            resolve("geek");
-        }, 100);
+  const p1 = Promise.resolve(50);
+  const p2 = 200;
+  const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("testing");
+    }, 100);
+  });
+  Promise.all([p1, p2, p3]).then((result) => {
+    console.log(result);
+  });
+
+  const p4 = Promise.resolve(50);
+  const p5 = new Promise((resolve, reject) => setTimeout(reject, 100, "testing"));
+  const prm = [p4, p5];
+
+  Promise.allSettled(prm).then((results) =>
+    results.forEach((result) => {
+        console.log('results');
+      console.log(results);
     })
-    Promise.all([p1, p2, p3]).then((values)=>{
-        console.log(values);
-    }).catch((error)=>{
-        console.log(error);
-    });
-}
+  );
+};
